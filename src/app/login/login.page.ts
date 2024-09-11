f4e981d90d3570489daaedefee607df7c63fbdba
BastianNoMas <bastian.ihh@gmail.com>
2024-09-11 01:24:54 -0300

    Index2
    Se modifican detalles de index2

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';  // Importa Router
 
 @Component({
   selector: 'app-login',
@@ -10,18 +11,20 @@ export class LoginPage implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor() { }
+  constructor(private router: Router) { }  // Inyecta Router
 
   ngOnInit() {
   }
 
   onLogin() {
     const usuarioGuardado = JSON.parse(localStorage.getItem('usuario') || '{}');
-    
+
     // Verificar si el usuario existe y las credenciales coinciden
     if (usuarioGuardado && usuarioGuardado.email === this.email && usuarioGuardado.password === this.password) {
       alert('Login exitoso');
+      
       // Redirigir a la página principal o dashboard
+      this.router.navigate(['/index2']);  // Redirige a la página deseada
     } else {
       alert('Correo o contraseña incorrectos');
     }
