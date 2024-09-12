65614af7b8974c676c2011bbcd277e5588f11930
BastianNoMas <bastian.ihh@gmail.com>
2024-09-12 20:11:53 -0300

    Validacion registro
    Se agrega validacion ts en registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -28,25 +28,32 @@ export class RegistroPage implements OnInit {
   }
 
   onRegister() {
-    // Guardar los datos en Local Storage
-    const usuario = {
-      nombre: this.nombre,
-      apellidoPaterno: this.apellidoPaterno,
-      apellidoMaterno: this.apellidoMaterno,
-      rut: this.rut,
-      email: this.email,
-      password: this.password,
-      preguntaSecreta: this.preguntaSecreta,
-      respuestaSecreta: this.respuestaSecreta
-    };
-
-    // Guardamos el objeto del usuario en Local Storage
-    localStorage.setItem('usuario', JSON.stringify(usuario));
-
-    // Mensaje de confirmación
-    alert('Registro exitoso. Ahora puede iniciar sesión.');
-    
-    // Redirigir a la página de inicio
-    this.router.navigate(['/home']);
+    // Verificar si algún campo está vacío
+    if (!this.nombre || !this.apellidoPaterno || !this.apellidoMaterno || !this.rut || !this.email || !this.password || !this.preguntaSecreta || !this.respuestaSecreta) {
+      // Mostrar un mensaje de error si algún campo está vacío
+      alert('Por favor, complete todos los campos antes de registrar.');
+      return; // Detener la ejecución si hay campos vacíos
+    } else {
+      // Guardar los datos en Local Storage
+      const usuario = {
+        nombre: this.nombre,
+        apellidoPaterno: this.apellidoPaterno,
+        apellidoMaterno: this.apellidoMaterno,
+        rut: this.rut,
+        email: this.email,
+        password: this.password,
+        preguntaSecreta: this.preguntaSecreta,
+        respuestaSecreta: this.respuestaSecreta
+      };
+  
+      // Guardamos el objeto del usuario en Local Storage
+      localStorage.setItem('usuario', JSON.stringify(usuario));
+  
+      // Mensaje de confirmación
+      alert('Registro exitoso. Ahora puede iniciar sesión.');
+      
+      // Redirigir a la página de inicio
+      this.router.navigate(['/home']);
+    }
   }
-}
+}  
