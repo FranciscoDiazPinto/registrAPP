85f2cc1e99800d66bb4ca7fad7c7d74866c8c4f9
FranciscoDiazPinto <137012203+FranciscoDiazPinto@users.noreply.github.com>
2024-10-24 16:14:05 -0300

    api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private baseUrl: string = 'https://registr-api.fly.dev/user';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // crear usuario
+  createUser(userData: any): Observable<any> {
+    const url = `${this.baseUrl}/create`;
+    return this.http.post(url, userData);
+  }
+
+  // login
+  loginUser(loginData: any): Observable<any> {
+    const url = `${this.baseUrl}/login`;
+    return this.http.post(url, loginData);
+  }
 }
