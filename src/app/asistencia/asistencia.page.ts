4569c8e232127a9c27cddb82aa842826e84e34dc
FranciscoDiazPinto <137012203+FranciscoDiazPinto@users.noreply.github.com>
2024-11-26 23:28:58 -0300

    Update asistencia.page.ts

--- src/app/asistencia/asistencia.page.ts
+++ src/app/asistencia/asistencia.page.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-
-
 @Component({
   selector: 'app-asistencia',
   templateUrl: './asistencia.page.html',
@@ -10,30 +7,9 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 })
 export class AsistenciaPage implements OnInit {
 
-  scannedData: string | null = null;
-
-  constructor(private barcodeScanner: BarcodeScanner) { }
+  constructor() { }
 
   ngOnInit() {
-    this.barcodeScanner.scan().then(barcodeData => {
-      console.log('Barcode data:', barcodeData);
-      this.scannedData = barcodeData.text;
-      // Aquí puedes llamar a un servicio para registrar la asistencia.
-      this.registerAttendance(this.scannedData);
-    }).catch(err => {
-      console.error('Error al escanear el código QR:', err);
-    });
   }
-
-  registerAttendance(data: string | null) {
-    if (data) {
-      // Aquí enviarías los datos al backend usando un servicio HTTP.
-      console.log('Registrando asistencia para:', data);
-    } else {
-      console.warn('No se detectó ningún dato para registrar.');
-    }
-  }
-
-
 
 }
