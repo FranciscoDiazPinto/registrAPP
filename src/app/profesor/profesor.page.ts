c933dc86facabc7143c18a64bff7b8bc75dfa9a1
BastianNoMas <bastian.ihh@gmail.com>
2024-11-27 20:05:06 -0300

    Generacion QR

--- src/app/profesor/profesor.page.ts
+++ src/app/profesor/profesor.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profesor',
   templateUrl: './profesor.page.html',
   styleUrls: ['./profesor.page.scss'],
 })
-export class ProfesorPage implements OnInit {
+export class ProfesorPage {
+  qrData: string = ''; // Contenido del QR
+  qrCodeUrl: string | null = null; // URL del QR generado
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  generateQR() {
+    if (this.qrData.trim() === '') {
+      alert('Por favor, ingresa el contenido para el QR.');
+      return;
+    }
+
+    // Construye la URL de la API con el contenido del QR
+    this.qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
+      this.qrData
+    )}`;
   }
-
 }
